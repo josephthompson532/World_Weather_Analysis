ccf172e9a018b7834f9a5f45f0109038a95986cd
josephthompson532 <66881241+josephthompson532@users.noreply.github.com>
2020-09-10 15:47:38 -0700

    initial commit

---
+++ Vacation_Search/config.py
@@ -0,0 +1,4 @@
+weather_api_key="f6888e15e825bcfaffdea654b33ea4ab"
+g_key = "AIzaSyC3VKOq8P9KQWanu2mSVsUoplkd1dM2XjI"
+DataFinder = '63tga4auyunaukxtfwvx0ug0'
+# the 'g_key' is the google maps API key
