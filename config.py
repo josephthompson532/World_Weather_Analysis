2c8bf7be34cc48a12e5a4389988a64b669555da2
Joseph Wallace Thompson <66881241+josephthompson532@users.noreply.github.com>
2020-09-10 16:05:55 -0700

    Delete config.py

--- config.py
+++
@@ -1,4 +0,0 @@
-weather_api_key="37a94047d5f4c734dfd49c775525e078"
-g_key = "AIzaSyC3VKOq8P9KQWanu2mSVsUoplkd1dM2XjI"
-DataFinder = '63tga4auyunaukxtfwvx0ug0'
-# the 'g_key' is the google maps API key
